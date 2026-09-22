12b7724ecd37b446737f7b2f5fa972210152c4df
itsmecheetah <enzoljost@gmail.com>
2026-09-21 18:29:34 -0700

    Fix style inconsistencies and make package unique from $all

--- common/starship.nix
+++ common/starship.nix
@@ -8,7 +8,8 @@
 			format = lib.concatStrings [
 					"[█](fg:#b16286)$directory[](fg:#b16286 bg:#689d6a)"
 					"$git_branch$git_status[](fg:#689d6a bg:#458588)"
-					"$c$cpp$rust$golang$nodejs$bun$php$java$kotlin$haskell$python[](fg:#458588)"
+					"[$c$cpp$rust$golang$nodejs$bun$php$java$kotlin$haskell$python](bg:#458588 fg:#282828)[](bg:#569699 fg:#458588)"
+					"$package[](fg:#569699)"
 					"\n$all$character"
 			];
 
@@ -25,6 +26,16 @@
 			git_status = {
 				format = "[[($all_status$ahead_behind )]($style)]($style)";
 				style = "bg:#689d6a fg:#282828";
+			};
+
+			package = {
+				format = "[ is $symbol$version]($style)";
+				style = "bg:#569699 fg:#282828";
+			};
+
+			rust = {
+				format = "[ $symbol($version) ]($style)";
+				style = "bg:#458588 fg:#282828";
 			};
 		};
 	};
